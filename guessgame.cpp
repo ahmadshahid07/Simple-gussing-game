24b32860edfd3230fc51dffccedcdc9c04bc7aa3
Ahmad shahid <ahmadshahid.45612@gmail.com>
2025-08-03 13:06:47 +0500

    Add files via upload

---
+++ guessgame.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int number = rand() % 100 + 1, guess, tries = 0;
+
+    cout << "Guess a number between 1 and 100: ";
+    do {
+        cin >> guess;
+        tries++;
+        if (guess > number)
+            cout << "Too high! Try again: ";
+        else if (guess < number)
+            cout << "Too low! Try again: ";
+        else
+            cout << "Correct! You guessed in " << tries << " tries.\n";
+    } while (guess != number);
+
+    return 0;
+}
